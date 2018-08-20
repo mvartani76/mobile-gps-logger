65228789e6a4c84210f31eae9aa8a00cd79054ea
Mike Vartanian <mike.vartanian@gmail.com>
2018-08-20 00:06:00 -0400

    Initial setup of settings viewcontroller. Currently can configure log file format and log interval.

--- iOS/gps-logger/gps-logger/SettingsViewController.swift
+++ iOS/gps-logger/gps-logger/SettingsViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    @IBOutlet weak var logFormatPicker: UIPickerView!
+    @IBOutlet weak var logIntervalLabel: UILabel!
+    @IBOutlet weak var logIntervalSlider: UISlider!
+    
+    var logFormatDataSource = ["gpx", "kml"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        logFormatPicker.delegate = self
+        logFormatPicker.dataSource = self
+        let currentSliderValue = Int(logIntervalSlider.value)
+        logIntervalLabel.text = "\(currentSliderValue)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return logFormatDataSource.count
     }
 
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return logFormatDataSource[row] as String
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(logFormatDataSource[row])
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        let currentValue = Int(sender.value)
+        logIntervalLabel.text = "\(currentValue)"
+    }
+    
 }
 
