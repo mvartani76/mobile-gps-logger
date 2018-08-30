33332d01db2ad60ee3e79c5348e296c0eecaaecc
mvartani76 <mike.vartanian@gmail.com>
2018-08-30 14:38:50 -0400

    Added getters/setters to SettingsDataViewModel.

--- Android/gpslogger/app/src/main/java/com/example/mikevartanian/gps_logger/SettingsDataViewModel.java
+++ Android/gpslogger/app/src/main/java/com/example/mikevartanian/gps_logger/SettingsDataViewModel.java
@@ -7,5 +7,37 @@ public class SettingsDataViewModel extends ViewModel {
     public String logFormat = "gpx";
     public int logInterval = 300;
     public String logMethod = "Time";
+    public int logButtonState = 1;
 
+    public String getLogFormat() {
+        return this.logFormat;
+    }
+
+    public int getLogInterval() {
+        return this.logInterval;
+    }
+
+    public String getLogMethod() {
+        return this.logMethod;
+    }
+
+    public int getLogButtonState() {
+        return this.logButtonState;
+    }
+
+    public void setLogFormat(String logFormat) {
+        this.logFormat = logFormat;
+    }
+
+    public void setLogInterval(int logInterval) {
+        this.logInterval = logInterval;
+    }
+
+    public void setLogMethod(String logMethod) {
+        this.logMethod = logMethod;
+    }
+
+    public void setLogButtonState(int logButtonState) {
+        this.logButtonState = logButtonState;
+    }
 }
